ca215215c973f2bf8101a270568d0745221ae318
Ivan Shipilov <ivanshipilov@gmail.com>
2021-06-06 15:29:10 +0500

    vue-input-time fix

--- src/components/InputTime.vue
+++ src/components/InputTime.vue
@@ -14,47 +14,42 @@ export default {
     return {
       time: "",
       input: {
-        hours: "0",
-        minutes: "0",
-        seconds: "0"
+        hours: null,
+        minutes: null,
+        seconds: null
       }
     }
   },
   computed: {
-    step () { return parseInt(this.input.seconds) > 0 ? 1 : 60 }
+    step () { return this.input.seconds ? 1 : 60 }
   },
   watch: {
     input: {
       deep: true,
       immediate: true,
       handler (val) {
-        let arr = [val.hours, val.minutes]
-        if (this.step == 1) {
-          arr.push(val.seconds)
+        if (val.hours && val.minutes) {
+          let arr = [val.hours, val.minutes]
+          if (val.seconds) arr.push(val.seconds)
+          this.time = arr.map(e => e.length == 2 ? e : "0" + e).join(":")
+        } else {
+          this.time = ""
         }
-        this.time = arr.map(e => e.length == 2 ? e : "0" + e).join(":")
       }
     }
   },
   methods: {
     setNow () {
-      let withSeconds = false
       let d = new Date()
-      if (parseInt(this.input.hours) == d.getHours() && parseInt(this.input.minutes) == d.getMinutes()) {
-        withSeconds = true
-      }
       this.input.hours = d.getHours().toString()
       this.input.minutes = d.getMinutes().toString()
-      if (withSeconds) {
-        this.input.seconds = d.getSeconds().toString()
-      }
+      if (this.input.seconds) this.input.seconds = d.getSeconds().toString()
     },
     setTime (value = "") {
-      let str = value.split(":")
-      let arr = ["0", "0", "0"].map((v, k) => str[k] && str[k].length > 0 ? str[k] : v)
-      this.input.hours = arr[0]
-      this.input.minutes = arr[1]
-      this.input.seconds = arr[2]
+      let arr = value.split(":")
+      this.input.hours = arr[0] || null
+      this.input.minutes = arr[1] || null
+      this.input.seconds = arr[2] || null
     }
   }
 }
